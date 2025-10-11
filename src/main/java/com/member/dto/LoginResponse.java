fc8d234ea52ff9327558c15f17d17e980d21e470
younlll <youn.lee96@gmail.com>
2025-10-11 18:12:20 +0900

    feat: kakao 사용자 필요 정보 받아오기 구현

---
+++ src/main/java/com/member/dto/LoginResponse.java
@@ -0,0 +1,36 @@
+package com.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class LoginResponse {
+
+	// LoginTokenResponse 토큰 정보
+	private String tokenType;
+	private String accessToken;
+	private Integer expiresIn;
+	private String refreshToken;
+	private Integer refreshTokenExpiresIn;
+
+	// KakaoUserInfoResponse 로그인 계정의 사용자 정보
+	private String kakaoId;
+	private String connectedAt;
+	private String email;
+
+	public static LoginResponse of(LoginTokenResponse loginTokenResponse, KakaoUserInfoResponse kakaoUserInfoResponse) {
+		return LoginResponse.builder()
+			.tokenType(loginTokenResponse.getTokenType())
+			.accessToken(loginTokenResponse.getAccessToken())
+			.expiresIn(loginTokenResponse.getExpiresIn())
+			.refreshToken(loginTokenResponse.getRefreshToken())
+			.refreshTokenExpiresIn(loginTokenResponse.getRefreshTokenExpiresIn())
+			.kakaoId(kakaoUserInfoResponse.getKakaoIdAsString())
+			.connectedAt(kakaoUserInfoResponse.getConnectedAt())
+			.email(kakaoUserInfoResponse.getKakaoAccount().getEmail())
+			.build();
+	}
+}
